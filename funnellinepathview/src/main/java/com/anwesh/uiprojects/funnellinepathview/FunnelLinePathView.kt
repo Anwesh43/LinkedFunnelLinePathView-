1fc261c3bc508231796c23c14c540b162371c252
Anwesh43 <anweshthecool0@gmail.com>
2020-10-03 02:37:43 +0530

    adding FunnelLinePathView class

--- funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
+++ funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
@@ -76,3 +76,19 @@ fun Canvas.drawFLPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawFunnelLinePath(scale, w, h, paint)
 }
+
+class FunnelLinePathView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
