cdd98d5e6f4c1d134fd81e089c6371e0810cabff
Anwesh43 <anweshthecool0@gmail.com>
2020-10-03 02:53:50 +0530

    adding Animator class

--- funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
+++ funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
@@ -111,4 +111,31 @@ class FunnelLinePathView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
