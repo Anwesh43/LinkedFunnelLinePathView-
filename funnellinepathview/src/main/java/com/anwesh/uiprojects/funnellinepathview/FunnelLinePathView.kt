ccf3ba128c216dc40672736a90c4a992ca1a7923
Anwesh43 <anweshthecool0@gmail.com>
2020-10-03 02:36:31 +0530

    adding Canvas extension methods

--- funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
+++ funnellinepathview/src/main/java/com/anwesh/uiprojects/funnellinepathview/FunnelLinePathView.kt
@@ -33,3 +33,46 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i  * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.to(end : Float, scale : Float) : Float = this + (end - this) * scale
+
+fun Canvas.drawFunnelLinePath(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = scale.divideScale(0, parts)
+    val sf2 : Float = scale.divideScale(1, parts)
+    val sf3 : Float = scale.divideScale(2, parts)
+    val sf4 : Float = scale.divideScale(3, parts)
+    val x : Float = w / wFactor
+    val y : Float = h / hFactor
+    val xGap : Float = w / 2 - x
+
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-w / 2, h / 2, - w / 2 + xGap * sf1, h / 2 - y * sf2, paint)
+        drawLine(-w / 2 + xGap, h / 2 - y, - w/ 2 + xGap, (h / 2 - y).to(-(h / 2 - y), sf2), paint)
+        drawLine(-w / 2 + xGap, (-h / 2 + y), -w / 2 + xGap + xGap * sf3, (-h / 2 + y).to(-h / 2, sf3), paint)
+        val path : Path = Path()
+        path.moveTo(0f, h / 2)
+        path.lineTo(- w / 2, h / 2)
+        path.lineTo(-w / 2 + xGap, h / 2 - y)
+        path.lineTo(-w / 2 + xGap, -h / 2 + y)
+        path.lineTo(-w / 2, -h / 2)
+        path.lineTo(0f, -h / 2)
+        path.lineTo(0f, h / 2)
+        clipPath(path)
+        drawRect(RectF(-w / 2, h / 2 - h * sf4, w / 2, h / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawFLPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFunnelLinePath(scale, w, h, paint)
+}
